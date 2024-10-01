9043e78dc3cc8aab0d24ee6fcee7feed512afb11
RAHUL KUMAR M S <rahulmanakyalath@gmail.com>
2024-10-01 19:52:50 +0530

    added implementer class

---
+++ src/Implementer.java
@@ -0,0 +1,46 @@
+import ClassBased.ComparableAndComparator;
+import DateManipulation.DateAndTime;
+
+public class Implementer {
+
+
+    public void implementer(){
+
+        ComparableAndComparator com = new ComparableAndComparator();
+
+//        com.comparator();
+//
+//        ArrayList<Student>stdList = new ArrayList<>();
+//        stdList.add(new Student(4,"Rahul"));
+//        stdList.add(new Student(3,"Athira"));
+//        stdList.add(new Student(1,"Sreehari"));
+//        stdList.add(new Student(2,"Vidhya"));
+//
+//        Collections.sort(stdList);
+//        System.out.println(stdList);
+//
+//        StreamOperations sp = new StreamOperations();
+//        sp.streamOp();
+//
+//        StringReverse stm=new StringReverse();
+//
+//        stm.stringManupulations();
+//        System.out.println(stm.reverse("Rahul"));
+
+//        LongestPalindromeSubstring lp = new LongestPalindromeSubstring();
+//
+//        lp.checkPalindromeString("malayala");
+//        lp.substring("englishs");
+
+//        HashsetBasics hb = new HashsetBasics();
+//
+//        hb.hashsetBasics();
+
+//        HashMapBasics hm = new HashMapBasics();
+//
+//        hm.hashmapTest();
+        DateAndTime at = new DateAndTime();
+
+        System.out.println(at.SmokerStatus("2023-7"));
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -15,45 +15,9 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
 
-        ComparableAndComparator com = new ComparableAndComparator();
+        Implementer im = new Implementer();
 
-//        com.comparator();
-//
-//        ArrayList<Student>stdList = new ArrayList<>();
-//        stdList.add(new Student(4,"Rahul"));
-//        stdList.add(new Student(3,"Athira"));
-//        stdList.add(new Student(1,"Sreehari"));
-//        stdList.add(new Student(2,"Vidhya"));
-//
-//        Collections.sort(stdList);
-//        System.out.println(stdList);
-//
-//        StreamOperations sp = new StreamOperations();
-//        sp.streamOp();
-//
-//        StringReverse stm=new StringReverse();
-//
-//        stm.stringManupulations();
-//        System.out.println(stm.reverse("Rahul"));
-
-//        LongestPalindromeSubstring lp = new LongestPalindromeSubstring();
-//
-//        lp.checkPalindromeString("malayala");
-//        lp.substring("englishs");
-
-//        HashsetBasics hb = new HashsetBasics();
-//
-//        hb.hashsetBasics();
-
-//        HashMapBasics hm = new HashMapBasics();
-//
-//        hm.hashmapTest();
-        DateAndTime at = new DateAndTime();
-
-        System.out.println(at.SmokerStatus("2023-7"));
-
-
-
+        im.implementer();
 
     }
 }
